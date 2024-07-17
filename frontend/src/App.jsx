5d31bb30032752e0e4eb57b7c2778e30847e51a6
Chanmin Jeon <chanminjeon@Chanmins-MBP.lan>
2024-07-16 18:36:07 -0600

    frontend in progress and some template files deleted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import LoginPage from './components/LoginPage'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+
+  const handleLogin = (event) => {
+    event.preventDefault()
+    console.log('username: ', username)
+    console.log('password: ', password)
+    setUsername('')
+    setPassword('')
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <LoginPage login={handleLogin} 
+        userChange={event => setUsername(event.target.value)}
+        passwordChange={event => setPassword(event.target.value)}
+        userVal={username} 
+        passwordVal={password}
+      />
     </>
   )
 }
